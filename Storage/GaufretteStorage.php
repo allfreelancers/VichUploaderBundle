d888d7171acad3afe1b28afeedc221ac15067a11
Eugene Dounar <eugene.dounar@intellectsoft.org>
2012-10-30 12:36:19 +0300

    Fix error with removing non-existent files

--- Storage/GaufretteStorage.php
+++ Storage/GaufretteStorage.php
@@ -61,8 +61,8 @@ class GaufretteStorage extends AbstractStorage
         $dst = $adapter->createFileStream($name);
 
         $src->open(new StreamMode('rb+'));
-        $dst->open(new StreamMode('ab+'));
-
+        $dst->open(new StreamMode('wb+'));
+        
         while (!$src->eof()) {
             $data    = $src->read(100000);
             $written = $dst->write($data);
